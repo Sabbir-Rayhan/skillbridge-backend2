098631fa111fbf7c85707f1319754b9eec009762
Mahee <sabbirrayhan475@gmail.com>
2026-03-31 00:35:28 +0600

    fix:trusted origin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,30 +1,33 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import {prisma} from './prisma'
+import { prisma } from "./prisma";
 
 //const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-    emailAndPassword: {
-        enabled: true,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "student",
+      },
+      status: {
+        type: "string",
+        required: false,
+        defaultValue: "active",
+      },
     },
-    user: {
-        additionalFields: {
-            role: {
-                type: "string",
-                required: false, 
-                defaultValue: "student",
-            },
-            status: {
-                type: "string",
-                required: false,
-                defaultValue: "active",
-            },
-        },
-    },
-    
-    trustedOrigins: ["http://localhost:3000"], 
+  },
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://skillbridge-topaz.vercel.app",
+  ],
 });
